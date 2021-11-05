a7529020bcce786491acb0283b294d77ce6b4c8e
Viacheslav-ua <atph@ukr.net>
2021-11-05 07:34:58 +0200

    st1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-import './styles.css'
+import "./styles.css";
+import thunk from "redux-thunk";
+import { increment, decrement, asyncIncrement } from "./redux/action";
+import { createStore, applyMiddleware } from "redux";
+import { rootReducer } from "./redux/rootReducer";
+
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
+
+const store = createStore(rootReducer, 0, applyMiddleware(thunk));
+// window.store = store;
+// console.log(store);
+
+addBtn.addEventListener("click", () => {
+  store.dispatch(increment());
+});
+
+subBtn.addEventListener("click", () => {
+  store.dispatch(decrement());
+});
+
+asyncBtn.addEventListener("click", () => {
+  store.dispatch(asyncIncrement());
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  counter.textContent = state;
+});
+
+store.dispatch({ type: "INIT_APPLICATION" });
+
+themeBtn.addEventListener("click", () => {
+  // document.body.classList.toggle("dark");
+});
